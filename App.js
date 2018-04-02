83543b6fe4da8fe87cbe37106892366c5e77a3ff
maacaro <maacaro@gmail.com>
2018-04-02 14:17:43 -0300

    FORECAST Save in localstorage last searchs

--- App.js
+++ App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import SearchTool from './components/SearchTool/SearchTool';
 import SearchResult from './components/SearchResult/SearchResult';
 import LastSearches from './components/LastSearches/LastSearches';
 import {getForecast} from './api/forecastAPI';
 
+export const putSearchAtFirstOf = (lastSearches,search)=>{
+  const nestLastSearch = lastSearches.slice(0,3)
+  nestLastSearch.unshift(search);
+  return nestLastSearch;
+}
 
 export default class App extends React.Component {
+
   state = {
-    cityForecast:{}
+    cityForecast:{},
+    lastSearches:[]
   }
-  _getForecast = async (city)=>{
+  componentDidMount = async () => {
+    try {
+    const lastSearches = JSON.parse(await AsyncStorage.getItem('lastSearches'));
+      if (lastSearches !== null){
+        this.setState({lastSearches});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+   _getForecast = async (city)=>{
     const cityForecast = await getForecast(city);
+    this.setState({cityForecast});
+  }
+  _onSearch = (inputSearch)=>{
+    this._getForecast(inputSearch);
+    this.setState({
+        lastSearches: putSearchAtFirstOf(this.state.lastSearches,inputSearch)
+      }, async ()=> {
+        try {
+          await AsyncStorage.setItem('lastSearches',JSON.stringify(this.state.lastSearches));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+    
   }
   render() {
     return (
       <View style={styles.container}>
-        <SearchTool onSearch ={(inputSearch)=> this._getForecast(inputSearch)} />
+        <SearchTool onSearch ={(inputSearch)=>this._onSearch(inputSearch)} />
         <SearchResult  data = {this.state.cityForecast} />
-        <LastSearches data = {[{city:"London"},{city:"Paris"}]}/>
+        <LastSearches data = {this.state.lastSearches.map(city=>({city}))}/>
       </View>
     );
   }
